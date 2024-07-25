ff4f07e199e8246ffc1b3971979eb8d5bfc1cde0
BarbaraZHAW <117282410+BarbaraZHAW@users.noreply.github.com>
2024-07-25 11:17:29 +0200

    Update Script-6.py

--- Script-6.py
+++ Script-6.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec  3 14:16:17 2023
-
 @author: Basia
 """
 
@@ -34,8 +32,8 @@ ACH = 1             # Air Chnages per Hour
 U_wall = 0.4        # W/K, overall heat transfer coeff. walls
 U_window = 3.5      # W/K, overall heat transfer coeff. windows
 
-θo, φo = 0.108, 0.8    # outdoor temperature & relative humidity
-θI, φI = 19, 0.5    # indoor temperature & relative humidity
+θo, φo = -3.8, 0.7    # outdoor temperature & relative humidity
+θI, φI = 24, 0.55    # indoor temperature & relative humidity
 wo = psy.w(θo, φo)
 wI = psy.w(θI, φI)
 
@@ -78,8 +76,8 @@ print(f'm = {m:.3f} kg/s')
 Kθ, Kw = 1e10, 0     # Kw can be 0
 β = 0.2              # by-pass factor
 m, mo = 3.5, 1.      # kg/s, mass flow rate: supply & outdoor (fresh) air
-θo, φo = 0.108, 0.8   # outdoor conditions
-θIsp, φIsp = 19., 0.5  # set point for indoor condition
+θo, φo = -3.8, 0.7   # outdoor conditions
+θIsp, φIsp = 24., 0.55  # set point for indoor condition
 
 #mi = 1.5           # kg/s, mass flow rate of infiltration air
 mi = 0.13792        # Data from expert
@@ -102,29 +100,4 @@ wd.interact(cool5.CAV_wd, θo=(0, 15), φo=(0.4, 1), θI=(19, 33), wI=(0.5, 1), 
 #            mi=(0.5, 3, 0.1), UA=(500, 800, 10), Qsaux=(0, 50_000, 25000), Qlaux=(0, 10_000, 3360), Qscab=(0, 60_000, 23000), Qlcab=(0, 20_000, 6280));
 
 
-#To implement ambient Temperature from excel (winter + summer)
-
-def read_temperature_data(file_path, sheet_name='Weather_Data-Zurich'):
-    try:
-        # Read the Excel file into a pandas DataFrame
-        df = pd.read_excel(file_path, sheet_name=sheet_name)
-
-        # Assuming the column with temperature data is named 'Temperature'
-        temperatures = df['T_amb_mean [°C]'].tolist()
-
-        return temperatures
-
-    except Exception as e:
-        print(f"Error reading temperature data from Excel: {e}")
-        return None
-
-# Example usage:
-file_path = r'C:\Users\hp\Documents\Project 1\Weather_data.xlsx'
-temperature_data = read_temperature_data(file_path)
-
-if temperature_data:
-    print("Temperature data:", temperature_data)
-else:
-    print("Failed to read temperature data.")
-θo = temperature_data
 
